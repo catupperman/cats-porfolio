82b4bf82f22d0a4d2595aa3a704f380b396b883b
catupperman <catupperman@gmail.com>
2022-01-24 17:22:15 -0500

    folder structure and notes on pages, psudo coding how I want everything to look.

---
+++ cats-portfolio/src/components/About-Me/About.js
@@ -0,0 +1,12 @@
+//Landing Page when a user initially comes to the site.  Simple paragraph about myself "elevator pitch", professional photo (waiting for Sarah to send over from the shoot).  Photo on right side of the page, and description of myself as a developer on the right side of the page. Plain background color.
+
+import React from 'react';
+import './About.css';
+
+function About() {
+    return(
+        <div></div>
+    )
+}
+
+export default About;
